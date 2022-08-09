ed0388067022fe77c528ab73f203c7e8541821c1
abyanmajid <abyanmjd@gmail.com>
2022-08-10 04:34:25 +0700

    configured urls

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home')
+	path('', views.home, name='home'),
+	path('products/', views.products, name='products'),
+	path('products/regular/', views.regular, name='regular'),
+	path('products/seasonal/', views.seasonal, name='seasonal'),
+	path('about', views.about, name='about'),
+	path('contact', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render, HttpResponse
 
 def home(response):
-    return HttpResponse('successful!!!')
+    return HttpResponse('home')
+
+def products(response):
+    return HttpResponse('products')
+
+def regular(response):
+    return HttpResponse('regular')
+
+def seasonal(response):
+    return HttpResponse('seasonal')
+
+def about(response):
+    return HttpResponse('about')
+
+def contact(response):
+    return HttpResponse('contact')

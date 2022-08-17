b84b47811c8db5af4d10abd8b0c77440df0b34ab
abyanmajid <abyanmjd@gmail.com>
2022-08-18 00:46:27 +0700

    design commit 1.2

--- sm/settings.py
+++ sm/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_FILES_DIR = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
 
 # Default primary key field type
